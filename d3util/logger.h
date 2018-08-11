2c23a571c4142f7513c3d7a14af3a7bedd0e04c9
André <d3roch4@gmail.com>
2018-08-11 19:43:03 -0300

    corrigido bug do paceholde

--- d3util/logger.h
+++ d3util/logger.h
@@ -10,7 +10,6 @@
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sinks.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/support/date_time.hpp>
 #include <boost/log/utility/setup/console.hpp>
