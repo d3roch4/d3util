9b1d88e83efa346703a3a092119793097e5e0f7c
André <d3roch4@gmail.com>
2019-08-02 17:50:22 -0300

    usando std::clog em vez de boost_log

--- d3util/logger.h
+++ d3util/logger.h
@@ -6,12 +6,19 @@
 #include <boost/log/core/core.hpp>
 #include <boost/log/trivial.hpp>
 
-#define  LOG_TRACE   BOOST_LOG_TRIVIAL(trace) << BOOST_CURRENT_FUNCTION << ": "
-#define  LOG_DEBUG   BOOST_LOG_TRIVIAL(debug) << BOOST_CURRENT_FUNCTION << ": "
-#define  LOG_INFO    BOOST_LOG_TRIVIAL(info)  << ": "
-#define  LOG_WARNING BOOST_LOG_TRIVIAL(warning) << BOOST_CURRENT_FUNCTION << ": "
-#define  LOG_ERROR   BOOST_LOG_TRIVIAL(error) << BOOST_CURRENT_FUNCTION << ": "
-#define  LOG_FATAL   BOOST_LOG_TRIVIAL(fatal) << BOOST_CURRENT_FUNCTION << ": "
+// #define  LOG_TRACE   BOOST_LOG_TRIVIAL(trace) << BOOST_CURRENT_FUNCTION << ": "
+// #define  LOG_DEBUG   BOOST_LOG_TRIVIAL(debug) << BOOST_CURRENT_FUNCTION << ": "
+// #define  LOG_INFO    BOOST_LOG_TRIVIAL(info)  << ": "
+// #define  LOG_WARNING BOOST_LOG_TRIVIAL(warning) << BOOST_CURRENT_FUNCTION << ": "
+// #define  LOG_ERROR   BOOST_LOG_TRIVIAL(error) << BOOST_CURRENT_FUNCTION << ": "
+// #define  LOG_FATAL   BOOST_LOG_TRIVIAL(fatal) << BOOST_CURRENT_FUNCTION << ": "
+
+#define  LOG_TRACE   std::clog << BOOST_CURRENT_FUNCTION << ": "
+#define  LOG_DEBUG   std::clog << BOOST_CURRENT_FUNCTION << ": "
+#define  LOG_INFO    std::clog  << ": "
+#define  LOG_WARNING std::cerr << BOOST_CURRENT_FUNCTION << ": "
+#define  LOG_ERROR   std::cerr << BOOST_CURRENT_FUNCTION << ": "
+#define  LOG_FATAL   std::cerr << BOOST_CURRENT_FUNCTION << ": "
 
 void init_logger(const std::string& prefixFile, boost::log::trivial::severity_level severity=boost::log::trivial::severity_level::info);
 
